d732bbba9598d52778f778d2b76f1c85716588af
Kyle Roach <kroach.work@gmail.com>
2017-05-09 21:17:12 -0400

    Portfolio headings

--- _scripts/index.js
+++ _scripts/index.js
@@ -2,4 +2,9 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   console.log('Ready')
+
+  $('.folio__title').click(e => {
+    $('.folio__title--active').removeClass('folio__title--active')
+    $(e.currentTarget).addClass('folio__title--active')
+  })
 })
