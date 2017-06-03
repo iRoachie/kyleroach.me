a91c4dadcbb4332f5e2bac80857b684883c4017a
Kyle Roach <kroach.work@gmail.com>
2017-06-03 18:06:26 -0400

    Working portfolio section with tiles and titles

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,10 +1,36 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  console.log('Ready')
-
-  $('.folio__title').click(e => {
-    $('.folio__title--active').removeClass('folio__title--active')
-    $(e.currentTarget).addClass('folio__title--active')
-  })
+  new KyleRoach()
 })
+
+class KyleRoach {
+  constructor() {
+    this.category = 'all'
+    this.initCategory()
+    this.selectCategory()
+  }
+
+  initCategory() {
+    $(`.folio__title[data-category=${this.category}]`).addClass('folio__title--active')
+    this.growSquares()
+  }
+
+  selectCategory() {
+    $('.folio__title').click(e => {
+      $('.folio__title--active').removeClass('folio__title--active')
+      $(e.currentTarget).addClass('folio__title--active')
+      this.category = $(e.currentTarget).attr('data-category')
+      this.growSquares()
+    })
+  }
+
+  growSquares() {
+    if (this.category === 'all') {
+      $('.folio-square').addClass('folio-square--visible')
+    } else {
+      $(`.folio-square:not([data-category=${this.category}])`).removeClass('folio-square--visible')
+      $(`.folio-square[data-category=${this.category}]`).addClass('folio-square--visible')
+    }
+  }
+}
