823bb3da8f04ff9df04f905e4df6da77fd1f04c6
Kyle <kroach.work@gmail.com>
2017-07-08 06:03:32 -0400

    Open source grid with items pulled from Github

--- _scripts/index.js
+++ _scripts/index.js
@@ -5,8 +5,10 @@ $(document).ready(() => {
 class KyleRoach {
   constructor() {
     this.grid = ''
+    this.osProjects = $('.os__projects')
     this.selectCategory()
     this.initGrid()
+    this.fetchRepos()
   }
 
   selectCategory() {
@@ -30,6 +32,42 @@ class KyleRoach {
       masonry: {
         isFitWidth: true,
       },
+    })
+  }
+
+  fetchRepos() {
+    const projects = [
+      'iRoachie/react-native-material-tabs',
+      'cooperka/react-native-snackbar',
+      'DefinitelyTyped/DefinitelyTyped',
+      'xotahal/react-native-material-ui',
+      'xinthink/react-native-material-kit',
+      '24ark/react-native-step-indicator',
+      'hectahertz/react-native-material-dialog',
+      'maxs15/react-native-modalbox'
+    ]
+    projects.map(repo => this.fetchRepo(repo))
+  }
+
+  fetchRepo(projectName) {
+    fetch(`https://api.github.com/repos/${projectName}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      this.osProjects.append(`
+        <div class="col-sm-6 col-md-4">
+          <article class="os-project">
+            <div>
+              <a href="${data.html_url}" target="_blank" class="os-project__title">${data.name}</a>
+              <p class="os-project__desc">${data.description}</p>
+            </div>
+            <div class="os-project__stats">
+              <span class="os-project__language ${data.language.toLowerCase()}">${data.language}</span>
+              <span class="os-project__stars"><i class="ion-android-star"></i> ${data.stargazers_count}</span>
+            </div>
+          </article>
+        </div>
+      `)
     })
   }
 }
